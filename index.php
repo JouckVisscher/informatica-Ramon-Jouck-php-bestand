4ec0899187df3fdb7bfb2e2f7a2f1ad8c39632c9
JouckVisscher <24477@leerling.pieterzandt.nl>
2026-06-12 08:44:40 +0000

    Website verplaatst

--- index.php
+++ index.php
@@ -1,13 +1,70 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <title>PHP & SQL CodeSpace</title>
-    <meta type="charset" value="utf-8">
-</head>
+<title>W3.CSS</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/5/w3.css">
 <body>
-    <h1>Welkom in de PHP & SQL CodeSpace!</h1>
-    <p>
-        Om het juiste bestand te openen, type je het pad naar dat bestand achter de URL die eindigt op .github.dev. Bijvoorbeeld: /oefenen/onderwerp-2/score.php.
-    </p>
+
+<div class="w3-container w3-olive">
+    <img src="logo.png" alt="Person" style="padding:7px;width:110px" class="w3-round-xlarge">
+
+
+
+
+<div class="w3-display-topright">
+
+  <div class="w3-circle" style="width:85px">
+    <img src="img_avatar3.png"  alt="Person" style="padding:7px;width:85px" class="w3-circle" onclick="toggleMenu()"  >
+  
+<div class="w3-sidebar w3-bar-block w3-collapse w3-card w3-animate-right w3-text-black" style="width:200px;right:0;" id="mySidebar">
+  <a href="./index.php" class="w3-bar-item w3-button">Homepagina </a>
+  <a href="./registreren.htm" class="w3-bar-item w3-button">Registreren	</a>
+  <a href="./inloggen.htm" class="w3-bar-item w3-button">Inloggen</a>
+  <a href="./instellingen.htm" class="w3-bar-item w3-button">Instellingen</a>
+</div>
+
+
+
+
+
+
+</div>
+
+
+
+<div class="w3-container">
+
+</div>
+
+</div>
+
+</div>
+
+ <div class="w3-container w3-light-grey">
+
+
+
+
+<a href= "./chinese.htm" class="w3-bar-item w3-button w3-round-large"><h1>chinees</h1> 
+<img src="general-tso-chicken.webp" class="w3-round-large" width="400px" height="300px>
+<a href="./chinese.htm" class="image-button">
+    <span> </span>
+</a>
+</a>
+
+
+<script>
+function toggleMenu() {
+  document.getElementById("mySidebar").classList.toggle("w3-show");
+}
+
+function w3_open() {
+  document.getElementById("mySidebar").style.display = "block";
+}
+
+function w3_close() {
+  document.getElementById("mySidebar").style.display = "none";
+}
+</script>
 </body>
-</html>
+</html> 
